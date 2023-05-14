2e43caffb11b54252f43ee0fda5e3025cbe576a7
wittmann1st <wittmann1st@gmail.com>
2023-05-14 18:07:22 -0300

    Método toString - POO Herança e Polimorfismo.

--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/model/Pessoa.java
@@ -42,4 +42,17 @@ public class Pessoa {
     public void setTelefoneContato(String telefoneContato) {
         this.telefoneContato = telefoneContato;
     }
+
+
+    //To String - Para textos - Para Strings de Caracteres
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneContato='" + telefoneContato + '\'' +
+                '}';
+    }
 }
