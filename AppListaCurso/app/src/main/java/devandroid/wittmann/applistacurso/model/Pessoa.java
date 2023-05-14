5a404478d6509fc9b646b2a401ad998114957470
wittmann1st <wittmann1st@gmail.com>
2023-05-14 17:59:47 -0300

    Uso dos métodos Get e Set.

--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/model/Pessoa.java
@@ -2,15 +2,44 @@ package devandroid.wittmann.applistacurso.model;
 
 public class Pessoa {
 
-    private String Nome;
-    private String Sobrenome;
-    private String Curso;
-    private String Telefone;
-
+    // Atributos - Objeto - Molde - Modelo - Template
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
 
     // Construtor
-    public Pessoa() {
+    public Pessoa() {}
 
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
 
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
     }
 }
--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
@@ -10,6 +10,9 @@ import devandroid.wittmann.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,37 @@ public class MainActivity extends AppCompatActivity {
 
         pessoa = new Pessoa();
 
+        // Atribuir conteúdo, dados, valores para o Objeto
+        pessoa.setPrimeiroNome("Roberto");
+        pessoa.setSobreNome("Wittmann");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11 1 1111 1111");
 
+        outraPessoa = new Pessoa();
+
+        outraPessoa.setPrimeiroNome("Carmela");
+        outraPessoa.setSobreNome("Laurenzana");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("22 2 2222 2222");
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
     }
 }
