0fefea60b30aff38cbd0931762a500dcf756eea2
wittmann1st <wittmann1st@gmail.com>
2023-05-20 11:50:45 -0300

    SharedPreferences buscar os dados salvos.

--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
@@ -44,32 +44,24 @@ public class MainActivity extends AppCompatActivity {
         controller.toString();
 
         pessoa = new Pessoa();
-
-        outraPessoa = new Pessoa();
-
-        pessoa.setPrimeiroNome("Roberto");
-        pessoa.setSobreNome("Wittmann");
-        pessoa.setCursoDesejado("Android");
-        pessoa.setTelefoneContato("11 1 1111 1111");
-
-        outraPessoa.setPrimeiroNome("Carmela");
-        outraPessoa.setSobreNome("Laurenzana");
-        outraPessoa.setCursoDesejado("Java");
-        outraPessoa.setTelefoneContato("22 2 2222 2222");
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome","NA"));
+        pessoa.setSobreNome(preferences.getString("sobreNome","NA"));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso","NA"));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato","NA"));
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
         editSobrenomeAluno = findViewById(R.id.editSobrenomeAluno);
         editNomeCurso = findViewById(R.id.editNomeCurso);
         editTelefoneContato = findViewById(R.id.editTelefoneContato);
 
-        btnLimpar = findViewById(R.id.btnLimpar);
-        btnSalvar = findViewById(R.id.btnSalvar);
-        btnFinalizar = findViewById(R.id.btnFinalizar);
-
         editPrimeiroNome.setText(pessoa.getPrimeiroNome());
         editSobrenomeAluno.setText(pessoa.getSobreNome());
         editNomeCurso.setText(pessoa.getCursoDesejado());
         editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
 
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
-        Log.i("POOAndroid", "Objeto outraPessoa:" + outraPessoa.toString());
+
 
     }
 }
