658780b7c0faf2b9b8d20a7957f1c57aa2c908ca
wittmann1st <wittmann1st@gmail.com>
2023-05-29 20:04:10 -0300

    Método dadosParaSpinner

--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
@@ -10,14 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 import devandroid.wittmann.applistacurso.R;
+import devandroid.wittmann.applistacurso.controller.CursoController;
 import devandroid.wittmann.applistacurso.controller.PessoaController;
+import devandroid.wittmann.applistacurso.model.Curso;
 import devandroid.wittmann.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     PessoaController controller;
+    CursoController cursoController;
     Pessoa pessoa;
     Pessoa outraPessoa;
+    List<Curso> listaDeCursos;
 
     EditText editPrimeiroNome;
     EditText editSobrenomeAluno;
@@ -35,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
         controller = new PessoaController(MainActivity.this);
         controller.toString();
+
+        cursoController = new CursoController();
+        listaDeCursos = cursoController.getListaCurso();
 
         pessoa = new Pessoa();
         controller.buscar(pessoa);
