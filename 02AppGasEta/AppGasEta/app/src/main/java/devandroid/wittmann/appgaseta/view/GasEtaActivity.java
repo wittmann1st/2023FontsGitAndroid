6f273cc0cc8e42855510c81c7d8dd264768aef24
wittmann1st <wittmann1st@gmail.com>
2023-06-24 18:49:42 -0300

    Configurado Telas Splash e GasEta.

---
+++ 02AppGasEta/AppGasEta/app/src/main/java/devandroid/wittmann/appgaseta/view/GasEtaActivity.java
@@ -0,0 +1,18 @@
+package devandroid.wittmann.appgaseta.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.wittmann.appgaseta.R;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+    }
+}
