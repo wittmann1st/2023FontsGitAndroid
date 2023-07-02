f91b84305be1328bb0803425d001011ad01590c3
wittmann1st <wittmann1st@gmail.com>
2023-07-02 11:41:38 -0300

    Removendo Classes e Métodos não utilizados.

---
+++ 02AppGasEta/AppGasEta/app/src/main/java/devandroid/wittmann/appgaseta/view/SplashActivity.java
@@ -0,0 +1,43 @@
+package devandroid.wittmann.appgaseta.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import devandroid.wittmann.appgaseta.R;
+import devandroid.wittmann.appgaseta.database.GasEtaDB;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        comutarTelasplash();
+
+    }
+
+    private void comutarTelasplash() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                GasEtaDB db = new GasEtaDB(SplashActivity.this);
+
+                Intent telaPrincipal = new Intent(
+                        SplashActivity.this,
+                        GasEtaActivity.class);
+
+                startActivity(telaPrincipal);
+                finish();
+
+            }
+        }, TIME_OUT_SPLASH);
+    }
+}
